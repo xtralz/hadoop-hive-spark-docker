2c98bec160b09ba8506921c986c1937f75f8b658
xtralz <ubuntu@ip-172-31-35-246.us-west-2.compute.internal>
2023-01-23 07:26:58 +0000

    Secure Jupyter

---
+++ debug/jupyterpasswd.sh
@@ -0,0 +1,6 @@
+python3
+
+# from notebook.auth import passwd
+# from notebook.auth.security import passwd_check
+# passwd('Password_for_Jupyter',algorithm='sha1')
+# passwd_check('sha1:xxxxxxxxxxxxxxxxxxxxx','Password_for_Jupyter')
